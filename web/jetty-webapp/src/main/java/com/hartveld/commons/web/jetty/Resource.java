d4d8c6a22d837b92e07d753a293ac72efdc18fd5
David Hartveld <david@hartveld.com>
2013-07-18 18:25:46 -0500

    Added several default operations for Resource.

--- web/jetty-webapp/src/main/java/com/hartveld/commons/web/jetty/Resource.java
+++ web/jetty-webapp/src/main/java/com/hartveld/commons/web/jetty/Resource.java
@@ -4,7 +4,13 @@ import java.util.List;
 
 public interface Resource<DTO> {
 
-	public abstract DTO retrieve(final long id);
+	public abstract DTO create(DTO dto);
+
+	public abstract void remove(long id);
+
+	public abstract DTO update(long id, DTO dto);
+
+	public abstract DTO retrieve(long id);
 
 	public abstract List<DTO> retrieveAll();
 
